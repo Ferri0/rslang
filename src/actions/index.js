7fe3fa2f74b9a7e8c0672fdb9c04697c1f5ca14f
IgorAleks88 <igor.alexeyenko@gmail.com>
2021-03-24 11:31:38 +0200

    add authorization actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,16 @@
-// const someAction = (value) => ({
-//   type: 'SOME_ACTION',
-//   payload: value
-// });
+const setShowAuth = (value) => ({
+  type: 'SET_SHOW_AUTH',
+  payload: value,
+});
+
+const setAuthorized = (value) => ({
+  type: 'SET_AUTHORIZED',
+  payload: value,
+});
+
+const setCurrentUser = (value) => ({
+  type: 'SET_CURRENT_USER',
+  payload: value,
+});
+
+export { setShowAuth, setAuthorized, setCurrentUser };
