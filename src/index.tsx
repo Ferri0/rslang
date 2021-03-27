4bc05987f9c02e5fe7e600c6feac7f24f14ec93c
Denis <DenisOleksiuk95@gmail.com>
2021-03-27 19:49:32 +0200

    feat: init wordservice

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,24 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import './scss/base.scss';
 import { App } from './components/App';
+import { WordsService } from './services';
+import { Context } from './components/word-service-context';
 
+const wordsService = new WordsService();
+wordsService
+  .getWords()
+  .then((result) => {
+    console.log(result[5].audio);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Context.Provider value={wordsService}>
+        <App />
+      </Context.Provider>
     </Provider>
   </StrictMode>,
   document.getElementById('root')
---
+++ src/services/Words-service.ts
@@ -0,0 +1,36 @@
+interface ServiceWords {
+  apiWordsBase: string;
+  getResource(url: string): Promise<any>;
+  getWords(): Promise<any>;
+  getWord(id: string): Promise<any>;
+}
+
+interface Item {
+  id: string;
+}
+
+export class WordsService implements ServiceWords {
+  apiWordsBase = 'https://yaia-team-rslang-api.herokuapp.com/words';
+
+  getResource = async (url: string) => {
+    const res = await fetch(this.apiWordsBase);
+
+    if (!res.ok) {
+      throw new Error(
+        `Ooops, Could not fetch ${url}` + `, received ${res.status}`
+      );
+    }
+
+    return await res.json();
+  };
+
+  getWords = async () => {
+    return await this.getResource(this.apiWordsBase);
+  };
+
+  getWord = async (id: string) => {
+    const data = await this.getResource(this.apiWordsBase);
+    const res = data.find((item: Item) => item.id === id);
+    return res;
+  };
+}
