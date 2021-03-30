11d171072ca8a775de1f4bfabd4503df643ce1f4
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-03-31 00:56:33 +0300

    feat: add TextBook controls

--- src/components/Textbook/Textbook.tsx
+++ src/components/Textbook/Textbook.tsx
@@ -63,6 +63,21 @@ export function Textbook({ unit }: TextbookProps) {
         <div className={style.wordsBlock}>
           {words.filter((e: any, i: number) => i % 2 === 0)}
         </div>
+        <div className={style.pageControls}>
+          <button
+            className={[style.btn, style.btn_firstPage].join(' ')}
+          ></button>
+          <button
+            className={[style.btn, style.btn_prevPage].join(' ')}
+          ></button>
+          <div className={style.pageBlock}>1/30</div>
+          <button
+            className={[style.btn, style.btn_nextPage].join(' ')}
+          ></button>
+          <button
+            className={[style.btn, style.btn_lastPage].join(' ')}
+          ></button>
+        </div>
       </div>
     </div>
   );
