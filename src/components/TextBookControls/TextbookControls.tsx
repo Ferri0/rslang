66bc0d596d727717986005d0722f6286aac6455b
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-03-31 23:09:18 +0300

    feat: implement settings block styles

--- src/components/TextBookControls/TextbookControls.tsx
+++ src/components/TextBookControls/TextbookControls.tsx
@@ -13,22 +13,47 @@ export function TextbookControls() {
             ? [style.settingsBlock, style.settingsBlock_active].join(' ')
             : style.settingsBlock
         }
-        onClick={() => {
-          if (gamesActive) setGamesActive(!gamesActive);
-          setSettingsActive(!settingsActive);
-        }}
-      />
+      >
+        <div
+          className={style.settingsClickZone}
+          onClick={() => {
+            if (gamesActive) setGamesActive(!gamesActive);
+            setSettingsActive(!settingsActive);
+          }}
+        />
+        <div className={style.settingWrapper}>
+          <label className={style.container}>
+            <input type="checkbox" id="translate" />
+            <span className={style.checkmark}></span>
+          </label>
+          <div className={style.settingWrapperIcons}>
+            <div>{'Кнопки'}</div>
+            <div className={style.settingsIconStar} />
+            <div className={style.settingsIconDel} />
+          </div>
+        </div>
+        <div className={style.settingWrapper}>
+          <label className={style.container}>
+            <input type="checkbox" id="translate" />
+            <span className={style.checkmark}></span>
+          </label>
+          {'Перевод слов'}
+        </div>
+      </div>
       <div
         className={
           gamesActive
             ? [style.gamesBlock, style.gamesBlock_active].join(' ')
             : style.gamesBlock
         }
-        onClick={() => {
-          if (settingsActive) setSettingsActive(!settingsActive);
-          setGamesActive(!gamesActive);
-        }}
       >
+        <div
+          className={style.gamesClickZone}
+          onClick={() => {
+            if (settingsActive) setSettingsActive(!settingsActive);
+            setGamesActive(!gamesActive);
+          }}
+        />
         <div
           className={[style.gameIcon, style.gameIcon_game1].join(' ')}
           onClick={() => console.log(`Game 1, array: ${[]}`)}
