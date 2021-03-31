a600eddb1b71a033a305cf366c302258c5326a13
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-03-31 17:49:38 +0300

    feat: base styles for TextbookControls

--- src/components/TextBookControls/TextbookControls.tsx
+++ src/components/TextBookControls/TextbookControls.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './TextbookControls.module.scss';
 
 export function TextbookControls() {
+  const [settingsActive, setSettingsActive] = useState(false);
+  const [gamesActive, setGamesActive] = useState(false);
   return (
     <div className={style.btnsBlock}>
       <button className={[style.btn, style.btn_home].join(' ')} type="button" />
-      <div className={style.settingsBlock} />
-      <div className={style.gamesBlock} />
+      <div
+        className={
+          settingsActive
+            ? [style.settingsBlock, style.settingsBlock_active].join(' ')
+            : style.settingsBlock
+        }
+        onClick={() => {
+          setSettingsActive(!settingsActive);
+        }}
+      />
+      <div
+        className={
+          gamesActive
+            ? [style.gamesBlock, style.gamesBlock_active].join(' ')
+            : style.gamesBlock
+        }
+        onClick={() => {
+          setGamesActive(!gamesActive);
+        }}
+      />
     </div>
   );
 }
