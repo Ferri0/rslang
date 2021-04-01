3d208c75f93eef9b1d15eccf6b573405d35c8873
IgorAleks88 <igor.alexeyenko@gmail.com>
2021-04-01 12:18:20 +0300

    add basic layout

--- src/components/Games/Own-game/Own-game.tsx
+++ src/components/Games/Own-game/Own-game.tsx
@@ -6,9 +6,17 @@ import style from './Own-game.module.scss';
 export const OwnGame = () => {
   return (
     <div className={style.ownGameWrapper}>
-      <Router>
-        <Link to="/">Back to home</Link>
-      </Router>
+      <div className={style.linkWrapper}>
+        <Router>
+          <Link to="/">Back to home</Link>
+        </Router>
+      </div>
+      <div className={style.healthPointsWrapper}>
+        This is health points wrapper
+      </div>
+      <div className={style.mainFieldWrapper}>This is Main Field wrapper</div>
+      <div className={style.taskWrapper}>This is Task wrapper</div>
+      <div className={style.taskBlocksWrapper}>This is Task Blocks wrapper</div>
     </div>
   );
 };
