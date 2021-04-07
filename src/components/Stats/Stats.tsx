9a799439cf7c595ecd0ca0ed1438e353c8fb452d
Ivan Tur <ivantur2@gmail.com>
2021-04-07 16:58:14 +0300

    feat: add stats ui

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,6 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './Stats.module.scss';
+import { useAction, useTypedSelector } from '../../hooks';
+import { Unit } from './Unit';
 
-export function Stats() {
-  return <div className={style.stats}>Stats</div>;
-}
+export const Stats: React.FC = () => {
+  const { day, allTime, series, loading } = useTypedSelector(
+    (state) => state.stats
+  );
+  const { fetchStats } = useAction();
+
+  useEffect(() => {
+    fetchStats();
+  }, []);
+
+  return (
+    <div className={style.root}>
+      <h2 className={style.title}>Статистика</h2>
+      <div className={style.wrapper}>
+        {loading ? (
+          'Загрузка...'
+        ) : (
+          <ul className={style.list}>
+            <Unit
+              text="Количество изученных слов сегодня"
+              value={day.learnedWords}
+            />
+            <Unit
+              text="Процент правильных ответов сегодня"
+              value={`${day.correctAnswers} %`}
+            />
+            <Unit
+              text="Количество изученных слов за все время"
+              value={allTime.learnedWords}
+            />
+            <Unit
+              text="Процент правильных ответов за все время"
+              value={`${allTime.learnedWords} %`}
+            />
+
+            <Unit
+              text="Самая длинная серия в игре Спринт"
+              value={series.sprint}
+            />
+            <Unit
+              text="Самая длинная серия в игре Саванна"
+              value={series.savanna}
+            />
+            <Unit
+              text="Самая длинная серия в игре Пазл"
+              value={series.puzzle}
+            />
+            <Unit
+              text="Самая длинная серия в игре Аудиовызов"
+              value={series.audiocall}
+            />
+          </ul>
+        )}
+        {/* <div>graphs</div> */}
+      </div>
+    </div>
+  );
+};
