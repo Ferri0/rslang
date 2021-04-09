03374c0c1853d512c00ab0c262f687c6a6221677
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-04-09 15:05:12 +0300

    fix: eslint error in Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './Footer.module.scss';
 
-export function Footer() {
+export function Footer(): React.ReactNode {
   return (
     <div className={style.footer}>
       2021
