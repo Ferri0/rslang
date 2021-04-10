923e857cfba65b875d4629262d631d066f80a780
Yaroslav Abrasimov <yabrasimov94@gmail.com>
2021-04-10 23:43:04 +0300

    fix: eslint errors in Words-service.ts

--- src/services/Words-service.ts
+++ src/services/Words-service.ts
@@ -6,20 +6,22 @@ export class WordsService implements ServiceWordsType {
   apiWords = (group = 0, page = 0): string =>
     `https://yaia-team-rslang-api.herokuapp.com/words?group=${group}&page=${page}`;
 
-  getResource = async (url: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getResource = async (url: string): Promise<any> => {
     const res = await fetch(url);
 
     if (!res.ok) {
-      throw new Error(
-        `Ooops, Could not fetch ${url}` + `, received ${res.status}`
-      );
+      throw new Error(`Ooops, Could not fetch ${url}, received ${res.status}`);
     }
 
     return res;
   };
 
-  getWords = async () => (await this.getResource(this.apiWords())).json();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getWords = async (): Promise<any> =>
+    (await this.getResource(this.apiWords())).json();
 
-  getSound = async (path: string) =>
-    await this.getResource(`${this.apiBaseUrl}${path}`);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getSound = async (path: string): Promise<any> =>
+    this.getResource(`${this.apiBaseUrl}${path}`);
 }
